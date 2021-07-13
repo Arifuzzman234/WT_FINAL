bc694739b0ca4dea50aa5d561c018b25d1f8c7f6
Arifuzzman niaz <niazarifuzzman99@gmail.com>
2021-07-13 12:03:28 +0600

    assignment

---
+++ assignment/Views/AdminHeader.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+	<body align = "center">
+	    <div>
+		    <h3>Admin</h3>
+		</div>
+		<div>
+			<a href = "http://localhost:7882/WT_FINAL/assignment/views/AllCategories.php" >All Categories</a>
+			<a href = "http://localhost:7882/WT_FINAL/assignment/views/AddCategory.php" >Add Category</a>
+			<a href = "http://localhost:7882/WT_FINAL/assignment/views/Login.php" >Logout</a>
+		</div>
+	</body>
+</html>
